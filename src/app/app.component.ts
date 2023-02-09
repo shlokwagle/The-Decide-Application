5a4f3da357feb1a49f8282f8f6c107c9572ce34f
shlokwagle <shlokwagle@gmail.com>
2023-02-09 10:42:26 -0800

    added third component
    added functionality to "don't like" it "start over" button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,67 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import answersValues from './utils/answers.json';
 
-
-export interface AppPage{
+export interface AppPage {
   visiblePanel: string;
-    question: string;
-    mode: boolean;
-    answers: string[];
-    custom: string[];
-    answer: string[]
+  question: string;
+  mode: boolean;
+  answers: string[];
+  custom: string[];
+  answer: string;
 }
 
-export interface Errors{
-  show:boolean,
-  message:string
+export interface Errors {
+  show: boolean;
+  message: string;
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
-
-  errors={
-    show:false,
-    message:null,
-  }
-
+export class AppComponent implements OnInit {
+  errors = {
+    show: false,
+    message: null,
+  };
 
   title = 'The Decide App';
-  appPage:AppPage= {
+  appPage: AppPage = {
     visiblePanel: 'one',
     question: '',
     mode: false,
     answers: answersValues.answers,
     custom: [],
-    answer: []
-  }
+    answer: '',
+  };
 
-  handleNext(values: AppPage){
+  handleNext(values: AppPage) {
     this.appPage = values;
   }
 
-  handleErros(values: Errors){
+  handleErros(values: Errors) {
     const errorsCopy = this.errors;
     this.errors = {
       show: values.show,
-      message: values.message
+      message: values.message,
     };
     setTimeout(() => {
-      this.errors = errorsCopy}, 2000
-    );
-
+      this.errors = errorsCopy;
+    }, 2000);
   }
-  ngOnInit(){}
+
+  handleReset() {
+    let appPageCopy = this.appPage;
+    appPageCopy = {
+      ...appPageCopy,
+      visiblePanel: 'one',
+      question: '',
+      mode: false,
+      custom: [],
+      answer: '',
+    };
+    this.appPage = appPageCopy;
+  }
+  ngOnInit() {}
 }
--- src/app/three/three.component.ts
+++ src/app/three/three.component.ts
@@ -1,13 +1,34 @@
-import { Component,  OnInit, Output, EventEmitter, Input  } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { AppPage } from '../app.component';
 
 @Component({
   selector: 'app-three',
   templateUrl: './three.component.html',
-  styleUrls: ['./three.component.css']
+  styleUrls: ['./three.component.css'],
 })
-export class ThreeComponent implements OnInit{
-@Input() appPage:AppPage;
-ngOnInit(){}
-}
+export class ThreeComponent implements OnInit {
+  @Input() appPage: AppPage;
+  @Output() next = new EventEmitter<AppPage>();
+  @Output() resetValues = new EventEmitter<boolean>();
 
+  constructor() {}
+
+  showResults() {
+    const appPageCopy = this.appPage;
+    const answersArray = this.appPage.answers;
+    const result: string =
+      answersArray[Math.floor(Math.random() * answersArray.length)];
+    appPageCopy.answer = result;
+    this.next.emit(appPageCopy);
+  }
+  doItAgain() {
+    this.showResults();
+  }
+
+  startOver() {
+    this.resetValues.emit(true);
+  }
+  ngOnInit() {
+    this.showResults();
+  }
+}
